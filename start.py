41661db11bcc97ebebf1e7f6a4135e0251c2b7af
Saksham <saksham.bhatla@gmail.com>
2016-06-11 17:07:14 -0400

    Multithreading works

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ import time
 import flask_server
 
 exitFlag = 0
-flaskObj=flask_server.main()
+#flaskObj=flask_server.main()
 
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -17,9 +17,7 @@ class myThread (threading.Thread):
             #sysinfo()
             i=0
 	    while (i <10):
-	        print "sysinfo thread\n" 
-	        time.sleep=5
-                i=i+1
+	        systeminfo()
         else:
             flask_server.main()
 	print "Exiting " + self.name
@@ -33,20 +31,25 @@ def print_time(threadName, delay, counter):
         counter -= 1
 
 
-def myfunc():
-    print "test threading!!!!!!!!!!!!!!!!!!!!!!!1"
+def systeminfo():
+    i=0
+    while i<100:
+        print "222222222test threading!!!!!!!!!!!!!!!!!!!!!!!1"
+        time.sleep(5)
+        i=i+1
 
 # Create new threads
-#thread1 = myThread(1, "System Information Thread", 1)
-#thread2 = myThread(2, "Flask Server", 2)
-t1=threading.Thread(target=flask_server.main)
-t2=threading.Thread(target=myfunc)
-t1.start()
-t2.start()
+thread1 = myThread(1, "System Information Thread", 1)
+thread2 = myThread(2, "Flask Server", 2)
+#t1=threading.Thread(target=flask_server.main)
+#t2=threading.Thread(target=myfunc)
+#t2.start()
+#print "thread 1 started. thread 2 starting now."
+#t1.start()
 
 
 # Start new Threads
-#thread1.start()
-#thread2.start()
+thread1.start()
+thread2.start()
 
 print "Exiting Main Thread"
